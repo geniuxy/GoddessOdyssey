a8160d0cd7cc57262f381e9b6f6263f8dbe9e4ad
geniuxy <2238384910@qq.com>
2025-02-27 00:16:08 +0800

    [25.2.26](K12) InputActionConfig

---
+++ Source/GoddessOdyssey/Public/DataAssets/Input/DataAsset_InputConfig.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "InputAction.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_InputConfig.generated.h"
+
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FGoddessInputActionConfig
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(Catagories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* InputAction;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class GODDESSODYSSEY_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = ( TitleProperty = "InputTag"))
+	TArray<FGoddessInputActionConfig> NativeInputActions;
+
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag);
+};
