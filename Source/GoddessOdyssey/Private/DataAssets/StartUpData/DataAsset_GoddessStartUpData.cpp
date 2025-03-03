40b51e87d75e58ee7a7ecf36d59d1da7bff115b4
geniuxy <2238384910@qq.com>
2025-03-04 00:43:32 +0800

    [25.3.4](K32) Hero ability set

--- Source/GoddessOdyssey/Private/DataAssets/StartUpData/DataAsset_GoddessStartUpData.cpp
+++ Source/GoddessOdyssey/Private/DataAssets/StartUpData/DataAsset_GoddessStartUpData.cpp
@@ -3,3 +3,22 @@
 
 #include "DataAssets/StartUpData/DataAsset_GoddessStartUpData.h"
 
+#include "AbilitySystems/BaseAbilitySystemComponent.h"
+
+void UDataAsset_GoddessStartUpData::GiveToAbilitySystemComponent(UBaseAbilitySystemComponent* InBaseASCToGiven,
+                                                                 int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InBaseASCToGiven, ApplyLevel);
+
+	for (const FGoddessAbilitySet& StartUpAbilitySet : GoddessStartUpAbilitySets)
+	{
+		if (!StartUpAbilitySet.IsValid()) continue;
+
+		FGameplayAbilitySpec AbilitySpec(StartUpAbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InBaseASCToGiven->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(StartUpAbilitySet.InputTag);
+
+		InBaseASCToGiven->GiveAbility(AbilitySpec);
+	}
+}
--- Source/GoddessOdyssey/Public/DataAssets/StartUpData/DataAsset_GoddessStartUpData.h
+++ Source/GoddessOdyssey/Public/DataAssets/StartUpData/DataAsset_GoddessStartUpData.h
@@ -3,8 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "AbilitySystems/Abilities/BaseGameplayAbility.h"
 #include "DataAssets/StartUpData/DataAsset_StartUpData.h"
 #include "DataAsset_GoddessStartUpData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FGoddessAbilitySet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(Catagories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UBaseGameplayAbility> AbilityToGrant;
+
+	bool IsValid() const
+	{
+		return InputTag.IsValid() && AbilityToGrant;
+	}
+};
 
 /**
  * 
@@ -13,5 +32,12 @@ UCLASS()
 class GODDESSODYSSEY_API UDataAsset_GoddessStartUpData : public UDataAsset_StartUpData
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void GiveToAbilitySystemComponent(UBaseAbilitySystemComponent* InBaseASCToGiven,
+	                                          int32 ApplyLevel = 1) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData", meta = (TitleProperty="InputTag"))
+	TArray<FGoddessAbilitySet> GoddessStartUpAbilitySets;
 };
