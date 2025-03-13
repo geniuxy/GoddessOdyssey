9318afa3e39321b5a02d9a374dc079364a99fe2d
geniuxy <2238384910@qq.com>
2025-03-14 01:35:29 +0800

    [25.3.14](K97) Update Weapon Icon

--- Source/GoddessOdyssey/Public/Components/UI/GoddessUIComponent.h
+++ Source/GoddessOdyssey/Public/Components/UI/GoddessUIComponent.h
@@ -6,6 +6,8 @@
 #include "Components/UI/BaseUIComponent.h"
 #include "GoddessUIComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChanged, TSoftObjectPtr<UTexture2D>, SoftWeaponIcon);
+
 /**
  * 
  */
@@ -13,8 +15,12 @@ UCLASS()
 class GODDESSODYSSEY_API UGoddessUIComponent : public UBaseUIComponent
 {
 	GENERATED_BODY()
+
 public:
 	// BlueprintAssignable 是虚幻引擎中用于 C++ 属性的修饰符，主要用于将多播委托（Multicast Delegate）暴露给蓝图
 	UPROPERTY(BlueprintAssignable)
 	FOnPercentChangedDelegate OnCurrentRageChanged;
+	
+	UPROPERTY(BlueprintCallable, BlueprintAssignable)
+	FOnEquippedWeaponChanged OnEquippedWeaponChanged;
 };
