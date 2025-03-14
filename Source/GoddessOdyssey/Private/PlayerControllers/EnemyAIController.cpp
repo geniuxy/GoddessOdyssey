344ce975c375a4296f47643175fa3b08b5200f06
geniuxy <2238384910@qq.com>
2025-03-14 21:25:20 +0800

    [25.3.14](K104) Enemy AI Perception

--- Source/GoddessOdyssey/Private/PlayerControllers/EnemyAIController.cpp
+++ Source/GoddessOdyssey/Private/PlayerControllers/EnemyAIController.cpp
@@ -5,6 +5,8 @@
 
 #include "DebugHelper.h"
 #include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 //FObjectInitializer：Unreal中用于初始化对象和子对象的工具，常见于构造函数中替换默认组件或修改属性。
 //将默认的路径跟随组件（UPathFollowingComponent）替换为 支持群体行为（Crowd Avoidance）的组件（UCrowdFollowingComponent）
@@ -13,4 +15,22 @@ AEnemyAIController::AEnemyAIController(const FObjectInitializer& ObjectInitializ
 {
 	if (UCrowdFollowingComponent* CrowdFollowingComponent = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
 		Debug::Print(TEXT("Path Following Component Valid"), FColor::Green);
+
+
+	EnemySenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("EnemySenseConfig_Sight"));
+	EnemySenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+	EnemySenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	EnemySenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+	EnemySenseConfig_Sight->SightRadius = 5000.f; // 能够看到目标的最大距离
+	EnemySenseConfig_Sight->LoseSightRadius = 0.f; // 失去目标视野的距离
+	EnemySenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f; // 设置 AI 的视野角度
+
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("EnemyPerceptionComponent"));
+	EnemyPerceptionComponent->ConfigureSense(*EnemySenseConfig_Sight);
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &AEnemyAIController::OnEnemyPerceptionUpdated);
+}
+
+void AEnemyAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
 }
--- Source/GoddessOdyssey/Public/PlayerControllers/EnemyAIController.h
+++ Source/GoddessOdyssey/Public/PlayerControllers/EnemyAIController.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "EnemyAIController.generated.h"
 
+class UAISenseConfig_Sight;
 /**
  * 
  */
@@ -15,5 +17,15 @@ class GODDESSODYSSEY_API AEnemyAIController : public AAIController
 	GENERATED_BODY()
 public:
 	AEnemyAIController(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* EnemyPerceptionComponent; // 允许 AI 通过不同的感官（如视觉、听觉、触觉等）感知周围环境中的刺激源
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* EnemySenseConfig_Sight; // 用于配置 AI 视觉感知的类
+
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
 	
 };
