47c879274dd8caff5fb4978c9bd4312981f0b522
geniuxy <2238384910@qq.com>
2025-03-15 22:53:03 +0800

    [25.3.15](K121) Send Enemy Attack event to actor

--- Source/GoddessOdyssey/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/GoddessOdyssey/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -3,9 +3,40 @@
 
 #include "Components/Combat/EnemyCombatComponent.h"
 
-#include "DebugHelper.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GoddessGameplayTags.h"
+#include "Abilities/GameplayAbilityTypes.h"
 
 void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
-	Debug::Print(GetOwner()->GetActorNameOrLabel() + TEXT(" is hitting ") + HitActor->GetActorNameOrLabel());
+	if (OverlappedActors.Contains(HitActor)) return;
+
+	OverlappedActors.AddUnique(HitActor);
+
+	// TODO block logic
+	bool bIsValidBlock = false;
+	const bool bIsPlayerBlocking = false;
+	const bool bIsMyAttackUnblockable = false;
+
+	if (bIsPlayerBlocking && !bIsMyAttackUnblockable)
+	{
+		//TODO::check if the block is valid
+	}
+
+	FGameplayEventData Data;
+	Data.Instigator = GetOwningPawn();
+	Data.Target = HitActor;
+	
+	if (bIsValidBlock)
+	{
+		//TODO::Handle successful block
+	}
+	else
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(
+			GetOwningPawn(),
+			GoddessGameplayTags::Shared_Event_MeleeAttack,
+			Data
+		);
+	}
 }
