e245d64a77be7ad7abb9a004b938920b172105a6
geniuxy <2238384910@qq.com>
2025-03-27 00:01:31 +0800

    cleancode

--- Source/GoddessOdyssey/Public/Components/UI/GoddessUIComponent.h
+++ Source/GoddessOdyssey/Public/Components/UI/GoddessUIComponent.h
@@ -11,7 +11,7 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEquippedWeaponChanged, TSoftObjec
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
 	FOnAbilityIconSlotUpdatedDelegate,
-	FGameplayTag, AbilityInputTag,
+	FGameplayTag, InAbilityInputTag,
 	TSoftObjectPtr<UMaterialInterface>, SoftAbilityIconMaterial
 );
 
