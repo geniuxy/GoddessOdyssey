a44ff3516aec12e8dfef49daf85b64c62e3720cd
geniuxy <2238384910@qq.com>
2025-03-27 22:40:00 +0800

    [25.3.27](K245) CountDownAction Class

---
+++ Source/GoddessOdyssey/Public/GoddessTypes/GoddessCountdownAction.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+enum class EGoddesssCountDownActionOutput : uint8;
+
+class FGoddessCountDownAction : public FPendingLatentAction
+{
+public:
+	FGoddessCountDownAction(float InTotalCountDownTime, float InUpdateInterval, float& InOutRemainingTime,
+	                        EGoddesssCountDownActionOutput& InCountDownOutput, const FLatentActionInfo& LatentInfo)
+		: bNeedToCancel(true)
+		  , TotalCountDownTime(InTotalCountDownTime)
+		  , UpdateInterval(InUpdateInterval)
+		  , OutRemainingTime(InOutRemainingTime)
+		  , CountDownOutput(InCountDownOutput)
+		  , ExecutionFunction(LatentInfo.ExecutionFunction)
+		  , OutputLink(LatentInfo.Linkage)
+		  , CallbackTarget(LatentInfo.CallbackTarget)
+		  , ElapsedInterval(0.f)
+		  , ElapsedTimeSinceStart(0.f)
+	{
+		
+	}
+
+private:
+	bool bNeedToCancel;
+	float TotalCountDownTime;
+	float UpdateInterval;
+	float& OutRemainingTime;
+	EGoddesssCountDownActionOutput& CountDownOutput;
+	FName ExecutionFunction;
+	int32 OutputLink;
+	FWeakObjectPtr CallbackTarget;
+	float ElapsedInterval;
+	float ElapsedTimeSinceStart;
+};
