0e3b6824f0fdb4e59c47af557358ecd9f458c2ce
geniuxy <2238384910@qq.com>
2025-03-28 21:44:48 +0800

    [25.3.28](K259) On Stone BeginOverlap

--- Source/GoddessOdyssey/Private/Item/PickUps/StoneBase.cpp
+++ Source/GoddessOdyssey/Private/Item/PickUps/StoneBase.cpp
@@ -2,3 +2,22 @@
 
 
 #include "Item/PickUps/StoneBase.h"
+
+#include "GoddessGameplayTags.h"
+#include "AbilitySystems/BaseAbilitySystemComponent.h"
+#include "Characters/Goddess.h"
+
+void AStoneBase::OnPickUpCollisionSphereBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	if (AGoddess* Goddess = Cast<AGoddess>(OtherActor))
+	{
+		Goddess->GetBaseAbilitySystemComponent()
+		       ->TryActivateAbilityByTag(GoddessGameplayTags::Character_Ability_PickUp_Stone);
+	}
+}
--- Source/GoddessOdyssey/Public/Item/PickUps/StoneBase.h
+++ Source/GoddessOdyssey/Public/Item/PickUps/StoneBase.h
@@ -10,4 +10,15 @@ UCLASS()
 class GODDESSODYSSEY_API AStoneBase : public APickUpBase
 {
 	GENERATED_BODY()
+
+protected:
+	
+	virtual void OnPickUpCollisionSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	) override;
 };
