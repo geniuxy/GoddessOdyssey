c7fca6a7cf8ad5dc22fd2b7f063f282f461e80c5
geniuxy <2238384910@qq.com>
2025-03-29 22:05:55 +0800

    [25.3.29](K274) Survival Game Mode

---
+++ Source/GoddessOdyssey/Private/GameModes/GoddessSurvivalGameMode.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameModes/GoddessSurvivalGameMode.h"
+
+void AGoddessSurvivalGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGoddessSurvivalGameMode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AGoddessSurvivalGameMode::SetCurrentSurvivalGameModeState(EGoddessSurvivalGameModeState InState)
+{
+	CurrentSurvivalGameModeState = InState;
+
+	OnSurvivalGameModeStateChangedDelegate.Broadcast(CurrentSurvivalGameModeState);
+}
---
+++ Source/GoddessOdyssey/Public/GameModes/GoddessSurvivalGameMode.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GoddessGameMode.h"
+#include "GoddessSurvivalGameMode.generated.h"
+
+
+UENUM(BlueprintType)
+enum class EGoddessSurvivalGameModeState:uint8
+{
+	WaitSpawnNewWave,
+	SpawningNewWave,
+	InProgress,
+	WaveCompleted,
+	AllWaveDone,
+	PlayerDied
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSurvivalGameModeStateChangedDelegate, EGoddessSurvivalGameModeState,
+                                            CurrentState);
+
+/**
+ * 
+ */
+UCLASS()
+class GODDESSODYSSEY_API AGoddessSurvivalGameMode : public AGoddessGameMode
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void SetCurrentSurvivalGameModeState(EGoddessSurvivalGameModeState InState);
+
+	UPROPERTY()
+	EGoddessSurvivalGameModeState CurrentSurvivalGameModeState;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnSurvivalGameModeStateChangedDelegate OnSurvivalGameModeStateChangedDelegate;
+};
