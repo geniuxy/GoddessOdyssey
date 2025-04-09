b487979ae2363a5e395e40a830cda62ba7f160ad
geniuxy <2238384910@qq.com>
2025-04-10 00:59:48 +0800

    给商店系统开了个头

---
+++ Source/GoddessOdyssey/Private/Characters/NPC.cpp
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/NPC.h"
+
+#include "Characters/Goddess.h"
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
+
+ANPC::ANPC()
+{
+	TalkCollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("PickUpCollisionSphere"));
+	TalkCollisionSphere->SetupAttachment(GetRootComponent());
+	TalkCollisionSphere->InitSphereRadius(50.f);
+	TalkCollisionSphere->
+		OnComponentBeginOverlap.AddUniqueDynamic(this, &ANPC::OnTalkCollisionSphereBeginOverlap);
+	TalkCollisionSphere->
+		OnComponentEndOverlap.AddUniqueDynamic(this, &ANPC::OnTalkCollisionSphereEndOverlap);
+
+	TalkWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("TalkWidget"));
+	TalkWidget->SetupAttachment(RootComponent);
+}
+
+void ANPC::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetTalkWidgetVisibility(false);
+}
+
+void ANPC::SetTalkWidgetVisibility(bool bWasVisible)
+{
+	if (TalkWidget)
+		TalkWidget->SetVisibility(bWasVisible);
+}
+
+void ANPC::OnTalkCollisionSphereBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	if (AGoddess* Goddess = Cast<AGoddess>(OtherActor))
+	{
+		SetTalkWidgetVisibility(true);
+	}
+}
+
+void ANPC::OnTalkCollisionSphereEndOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	if (AGoddess* Goddess = Cast<AGoddess>(OtherActor))
+	{
+		SetTalkWidgetVisibility(false);
+	}
+}
---
+++ Source/GoddessOdyssey/Public/Characters/NPC.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "NPC.generated.h"
+
+class UWidgetComponent;
+class USphereComponent;
+
+UCLASS()
+class GODDESSODYSSEY_API ANPC : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ANPC();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Shop Interaction")
+	USphereComponent* TalkCollisionSphere;
+
+	UPROPERTY(VisibleAnywhere, Category = "Shop Interaction")
+	UWidgetComponent* TalkWidget;
+
+	void SetTalkWidgetVisibility(bool bWasVisible);
+
+	UFUNCTION()
+	virtual void OnTalkCollisionSphereBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	UFUNCTION()
+	virtual void OnTalkCollisionSphereEndOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex
+	);
+
+public:
+};
