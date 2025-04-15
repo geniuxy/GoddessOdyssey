9cdf13803ee94b88df27c3c31bd8ee8270124a0b
geniuxy <2238384910@qq.com>
2025-04-15 22:23:05 +0800

    fix: 解决打到npc会崩溃的问题

--- Source/GoddessOdyssey/Private/Item/Weapons/Weapon.cpp
+++ Source/GoddessOdyssey/Private/Item/Weapons/Weapon.cpp
@@ -5,10 +5,12 @@
 
 #include "Components/BoxComponent.h"
 #include "GoddessFunctionLibrary.h"
+#include "Characters/BaseCharacter.h"
+#include "Characters/NPC.h"
 
 AWeapon::AWeapon()
 {
-	WeaponCollisionBox =CreateDefaultSubobject<UBoxComponent>(TEXT("WeaponCollisionBox"));
+	WeaponCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("WeaponCollisionBox"));
 	WeaponCollisionBox->SetupAttachment(GetRootComponent());
 	WeaponCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	WeaponCollisionBox->OnComponentBeginOverlap.AddUniqueDynamic(this, &AWeapon::OnWeaponBeginOverlap);
@@ -22,28 +24,31 @@ void AWeapon::AttachWeaponTo(USceneComponent* InParent, FName SocketName)
 }
 
 void AWeapon::OnWeaponBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+                                   UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                   const FHitResult& SweepResult)
 {
 	APawn* WeaponOwningPawn = GetInstigator<APawn>();
 
-	checkf(WeaponOwningPawn,TEXT("Forgot to assign an instiagtor as the owning pawn for the weapon: %s"),*GetName());
-	
-	if (APawn* HitPawn = Cast<APawn>(OtherActor))
+	checkf(WeaponOwningPawn, TEXT("Forgot to assign an instiagtor as the owning pawn for the weapon: %s"), *GetName());
+
+	if (ABaseCharacter* HitPawn = Cast<ABaseCharacter>(OtherActor))
 	{
+		if (Cast<ANPC>(OtherActor) != nullptr) return;
 		if (UGoddessFunctionLibrary::IsTargetPawnHostile(WeaponOwningPawn, HitPawn))
 			OnWeaponHitTarget.ExecuteIfBound(OtherActor);
 	}
 }
 
 void AWeapon::OnWeaponEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+                                 UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	APawn* WeaponOwningPawn = GetInstigator<APawn>();
 
-	checkf(WeaponOwningPawn,TEXT("Forgot to assign an instiagtor as the owning pawn for the weapon: %s"),*GetName());
+	checkf(WeaponOwningPawn, TEXT("Forgot to assign an instiagtor as the owning pawn for the weapon: %s"), *GetName());
 
-	if (APawn* HitPawn = Cast<APawn>(OtherActor))
+	if (ABaseCharacter* HitPawn = Cast<ABaseCharacter>(OtherActor))
 	{
+		if (Cast<ANPC>(OtherActor) != nullptr) return;
 		if (UGoddessFunctionLibrary::IsTargetPawnHostile(WeaponOwningPawn, HitPawn))
 			OnWeaponPulledFromTarget.ExecuteIfBound(OtherActor);
 	}
